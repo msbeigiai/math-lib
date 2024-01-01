5d714b28a6667c56a3e492eafd6502216bf802c5
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-01-01 23:55:40 +0200

    Update Point

--- src/main/java/com/msbeigi/base/Point.java
+++ src/main/java/com/msbeigi/base/Point.java
@@ -1,30 +1,21 @@
 package com.msbeigi.base;
 
-public class Point {
+public class Point extends Matrix {
     private double x;
     private double y;
 
-    public Point() {
-    }
+    public Point() {}
 
     public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+        super(2, 1);
+        X[0][0] = x;
+        X[1][0] = y;
+        this.x = X[0][0];
+        this.y = X[1][0];
     }
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
+    @Override
+    public String toString() {
+        return "[" + x + ", " + y + "]";
     }
 }
