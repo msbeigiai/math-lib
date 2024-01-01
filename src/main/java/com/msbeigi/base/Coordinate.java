a1ab2b79ed81ff0b4f7d159454a23951950b3e00
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-01-01 22:29:27 +0200

    Initial commit

---
+++ src/main/java/com/msbeigi/base/Coordinate.java
@@ -0,0 +1,5 @@
+package com.msbeigi.base;
+
+public abstract class Coordinate {
+
+}
---
+++ src/main/java/com/msbeigi/base/Point.java
@@ -0,0 +1,30 @@
+package com.msbeigi.base;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
