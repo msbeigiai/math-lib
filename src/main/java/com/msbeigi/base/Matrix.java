e9f26e4d1ec9dd677b739c582e2f86b1bc6224c1
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-01-01 23:29:34 +0200

    Create Matrix and Vector class

---
+++ src/main/java/com/msbeigi/base/Matrix.java
@@ -0,0 +1,9 @@
+package com.msbeigi.base;
+
+public class Matrix extends LinearAlgebra {
+    public Matrix(){}
+
+    public Matrix(int m, int n) {
+        super(m, n);
+    }
+}
---
+++ src/main/java/com/msbeigi/base/Vector.java
@@ -0,0 +1,13 @@
+package com.msbeigi.base;
+
+public class Vector extends LinearAlgebra {
+
+    public Vector() {
+    }
+
+    public Vector(int m) {
+        super(m, 1);
+    }
+
+
+}
