6c37ec19e31c882be390a4f94cc43ba7267fcbce
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-01-02 20:46:06 +0200

    Update Vector

--- src/main/java/com/msbeigi/base/Vector.java
+++ src/main/java/com/msbeigi/base/Vector.java
@@ -34,6 +34,32 @@ public class Vector extends LinearAlgebra {
         return vector;
     }
 
+    public Vector subtract(Vector v2) {
+        Vector vector = new Vector(length());
+        vector = v2.multiply(-1.0).add(this);
+        return vector;
+    }
+
+    public Vector multiply(Vector v2) {
+        Vector vector = new Vector(length());
+        if (this.length() != v2.length()) {
+            throw new IllegalArgumentException("Two vectors has not same length");
+        }
+        for (int i = 0; i < this.length(); i++) {
+            vector.X[i][0] = X[i][0] * v2.X[i][0];
+        }
+        return vector;
+    }
+    public Vector multiply(double x) {
+        Vector vector = new Vector(length());
+
+        for (int i = 0; i < this.length(); i++) {
+            vector.X[i][0] = x * X[i][0];
+        }
+        return vector;
+    }
+
+
     @Override
     public void printMat() {
         Print print = new Print(this);
