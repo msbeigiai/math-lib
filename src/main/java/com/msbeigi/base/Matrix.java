45f4c248d1f3bccbfa453074336550e64be1450f
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-01-04 20:30:43 +0200

    Update Matrix

--- src/main/java/com/msbeigi/base/Matrix.java
+++ src/main/java/com/msbeigi/base/Matrix.java
@@ -33,4 +33,48 @@ public class Matrix extends LinearAlgebra implements MatrixGeo {
         }
         return identity;
     }
+
+    @Override
+    public Matrix addition(Matrix other) {
+        if (other.m != this.m && other.n != this.n) {
+            throw new IllegalArgumentException("Matrices row and columns should be equal.");
+        }
+        Matrix matrix = new Matrix(m, n);
+        for (int i = 0; i < this.m; i++) {
+            for (int j = 0; j < this.n; j++) {
+                matrix.X[i][j] = this.X[i][j] + other.X[i][j];
+            }
+        }
+        return matrix;
+    }
+
+    @Override
+    public Matrix subtraction(Matrix other) {
+        return this.addition(other.multiplication(-1.0));
+    }
+
+    @Override
+    public Matrix multiplication(double scalar) {
+        Matrix matrix = new Matrix(this.m, this.n);
+        for (int i = 0; i < this.m; i++) {
+            for (int j = 0; j < this.n; j++) {
+                matrix.X[i][j] = this.X[i][j] * scalar;
+            }
+        }
+        return matrix;
+    }
+
+    @Override
+    public Matrix multiplication(Matrix other) {
+        if (this.n != other.m) {
+            throw new IllegalArgumentException("The first matrices column is not equal to other matrices row");
+        }
+        Matrix matrix = new Matrix(this.m, this.n);
+        for (int i = 0; i < this.m; i++) {
+            for (int j = 0; j < this.n; j++) {
+                matrix.X[i][j] += this.X[i][j] * other.X[j][i];
+            }
+        }
+        return matrix;
+    }
 }
